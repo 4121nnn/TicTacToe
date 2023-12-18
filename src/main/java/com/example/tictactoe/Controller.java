203b854ec0bfe7e3beecccaa3380fd05420d88d7
4121n <4121nnn@gmail.com>
2023-12-18 22:44:48 +0300

    animated grid and made simple animations for drawing X and 0. but not finish yet

--- src/main/java/com/example/tictactoe/Controller.java
+++ src/main/java/com/example/tictactoe/Controller.java
@@ -1,10 +1,22 @@
 package com.example.tictactoe;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
 
 public class Controller {
+    @FXML
+    AnchorPane rootPane;
     @FXML
     Label infoLabel;
 
@@ -16,7 +28,8 @@ public class Controller {
     String[] matrix = new String[LENGTH];
     String player ;
     int count = 0;
-
+    @FXML
+    Line topHorLine, bottomHorLine, leftVerLine, rightVerLine;
     public void initialize() {
         buttons[0] = btn1;
         buttons[1] = btn2;
@@ -35,19 +48,135 @@ public class Controller {
             buttons[i].setText("");
         }
         infoLabel.setText(player + " turn");
+        gridAnimation();
+        draw0(1);
+
     }
 
     public void startNewGame(){
         initialize();
+
     }
+    public void drawX(int i){
+        Line line1 = new Line(50, 50, 50, 50);
+        line1.setStroke(Color.BLUE);
+        line1.setStrokeWidth(3);
+
+        // Create the second diagonal line (from top-right to bottom-left)
+        Line line2 = new Line(100, 50, 100, 50);
+        line2.setStroke(Color.BLUE);
+        line2.setStrokeWidth(3);
+
+        // Add lines to the root pane
+        rootPane.getChildren().addAll(line1, line2);
+        Timeline timeline = new Timeline();
+
+        // Add a keyframe to the timeline
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(600),
+                new KeyValue(line1.endYProperty(),  50) ,
+                new KeyValue(line1.startYProperty(),  100),
+                new KeyValue(line1.endXProperty(),  50) ,
+                new KeyValue(line1.startXProperty(),  100),
+
+                new KeyValue(line2.endYProperty(),  50) ,
+                new KeyValue(line2.startYProperty(),  100),
+                new KeyValue(line2.endXProperty(),  100) ,
+                new KeyValue(line2.startXProperty(),  50)
+
+
+        );
+        // Add the keyframe to the timeline
+        timeline.getKeyFrames().add(keyFrame);
+
+        // Set the cycle count for the animation (indefinite for continuous animation)
+        //timeline.setCycleCount(Timeline.INDEFINITE);
+
+        // Start the animation
+        timeline.play();
+    }
+
+    public void draw0(int i){
+        // Create an arc (round line)
+        Arc arc = new Arc(70, 70, 38, 38, 0, 0);
+        arc.setFill(Color.TRANSPARENT);
+        arc.setStroke(Color.BLUE);
+        arc.setStrokeWidth(3);
+        arc.setType(ArcType.OPEN);
+
+        // Add the arc to the root pane
+        rootPane.getChildren().add(arc);
+
+        // Create a timeline for the animation
+        Timeline timeline = new Timeline();
+
+        // Add a keyframe to the timeline
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(600),
+                new KeyValue(arc.lengthProperty(), -360)
+        );
+
+        // Add the keyframe to the timeline
+        timeline.getKeyFrames().add(keyFrame);
+
+        // Play the animation
+        timeline.play();
+    }
+    public void gridAnimation(){
+        final int LINELENGTH = 150;
+        final double startX = 30.0;
+        final double startY = 30.0;
+        topHorLine.setStartX(startX);
+        topHorLine.setEndX(startX);
+        bottomHorLine.setStartX(startX);
+        bottomHorLine.setEndX(startX);
+
+        leftVerLine.setStartY(startY);
+        leftVerLine.setEndY(startY);
+        rightVerLine.setStartY(startY);
+        rightVerLine.setEndY(startY);
+        Timeline timeline = new Timeline();
+
+        // Add a keyframe to the timeline
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(700),
+                new KeyValue(topHorLine.endXProperty(), LINELENGTH + topHorLine.getEndX()) ,
+                new KeyValue(topHorLine.startXProperty(),  -(LINELENGTH - topHorLine.getStartX())),
+
+                new KeyValue(bottomHorLine.endXProperty(), LINELENGTH + bottomHorLine.getEndX()) ,
+                new KeyValue(bottomHorLine.startXProperty(),  -(LINELENGTH - bottomHorLine.getStartX())),
+
+                new KeyValue(leftVerLine.endYProperty(), LINELENGTH + leftVerLine.getEndY()) ,
+                new KeyValue(leftVerLine.startYProperty(),  -(LINELENGTH - leftVerLine.getStartY())),
+
+                new KeyValue(rightVerLine.endYProperty(), LINELENGTH + rightVerLine.getEndY()) ,
+                new KeyValue(rightVerLine.startYProperty(),  -(LINELENGTH - rightVerLine.getStartY()))
+
+        );
+        // Add the keyframe to the timeline
+        timeline.getKeyFrames().add(keyFrame);
+
+        // Set the cycle count for the animation (indefinite for continuous animation)
+        //timeline.setCycleCount(Timeline.INDEFINITE);
+
+        // Start the animation
+        timeline.play();
+    }
+
+
+
+
+
+
+
+
+
 
 
     private void onButtonPressed(int i){
 
-        if(!buttons[i].getText().isEmpty() || gameOver) return;
+        if(matrix[i].equals("X") || matrix[i].equals("0") || gameOver) return;
 
-        buttons[i].setText(player);
+        //buttons[i].setText(player);
         matrix[i] = player;
+        drawX(i);
         if(isWin()){
             gameOver = true;
             infoLabel.setText(player + " win!!!!");
@@ -58,6 +187,13 @@ public class Controller {
 
 
     }
+
+
+
+
+
+
+
     public boolean isWin(){
         return (matrix[0].equals(matrix[1]) && matrix[1].equals(matrix[2]) ||
                 matrix[3].equals(matrix[4]) && matrix[4].equals(matrix[5]) ||
