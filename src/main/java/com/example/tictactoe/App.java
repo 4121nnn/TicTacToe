1c390dac110a64de39ab634ee08b0538e805abfd
4121n <4121nnn@gmail.com>
2023-12-18 07:06:42 +0300

    added icon

--- src/main/java/com/example/tictactoe/App.java
+++ src/main/java/com/example/tictactoe/App.java
@@ -3,6 +3,7 @@ package com.example.tictactoe;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class App extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("TicTacToe");
         stage.setResizable(false);
+        stage.getIcons().add(new Image("D:\\41n\\java\\TicTacToe\\src\\main\\resources\\com\\example\\tictactoe\\tictactoe.png"));
         stage.setScene(scene);
         stage.show();
     }
