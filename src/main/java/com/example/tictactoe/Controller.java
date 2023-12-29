f170af6004d277cc05ddb8810bf8a2b68d213c2c
4121n <4121nnn@gmail.com>
2023-12-29 21:20:17 +0300

    added game result info at the end of game

--- src/main/java/com/example/tictactoe/Controller.java
+++ src/main/java/com/example/tictactoe/Controller.java
@@ -4,6 +4,7 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
@@ -17,72 +18,83 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.util.Duration;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Controller {
     @FXML
     AnchorPane rootPane;
 
     @FXML
-    Pane gridPane, X01, X02, X03, X04, X05, X06, X07, X08, X09;
+    Pane gridPane,infoPane, X01, X02, X03, X04, X05, X06, X07, X08, X09;
     @FXML
     Label infoLabel;
 
     @FXML
-    private Button  btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    private Button newGame, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
     private final int LENGTH = 9;
-    Button[] buttons = new Button[LENGTH];
+    private final int TOP = 20, BOTTOM = 280, LEFT = 50, MIDDLE = 150, RIGHT = 250 ;
+    Button[] buttons;
     Pane[] X0panes;
     boolean gameOver;
     String[] matrix = new String[LENGTH];
     String player ;
+
+    int[][] WIN_LINE = new int[8][4];
     int count = 0;
     @FXML
     Line topHorLine, bottomHorLine, leftVerLine, rightVerLine;
+
+    Set<Integer> isDraw ;
     public void initialize() {
-        buttons[0] = btn1;
-        buttons[1] = btn2;
-        buttons[2] = btn3;
-        buttons[3] = btn4;
-        buttons[4] = btn5;
-        buttons[5] = btn6;
-        buttons[6] = btn7;
-        buttons[7] = btn8;
-        buttons[8] = btn9;
+        buttons = new Button[]{btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9};
         X0panes = new Pane[]{X01, X02, X03, X04, X05, X06, X07, X08, X09};
         gameOver = false;
         player = "X";
-
-        for(int i = 0; i < matrix.length; i++){
-            matrix[i] = "z"+ i;
-
+        WIN_LINE[0] = new int[]{TOP, TOP, BOTTOM, BOTTOM};
+        WIN_LINE[1] = new int[]{BOTTOM, TOP, TOP, BOTTOM};
+        WIN_LINE[2] = new int[]{LEFT, TOP, LEFT, BOTTOM};
+        WIN_LINE[3] = new int[]{MIDDLE, TOP, MIDDLE, BOTTOM};
+        WIN_LINE[4] = new int[]{RIGHT, TOP, RIGHT, BOTTOM};
+        WIN_LINE[5] = new int[]{TOP, LEFT, BOTTOM, LEFT};
+        WIN_LINE[6] = new int[]{TOP, MIDDLE, BOTTOM, MIDDLE};
+        WIN_LINE[7] = new int[]{TOP, RIGHT, BOTTOM, RIGHT};
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = "z" + i;
         }
-        for(int i = 0; i < LENGTH; i++){
+        for (int i = 0; i < LENGTH; i++) {
             X0panes[i].getChildren().clear();
         }
+
+        infoPane.getChildren().clear();
         infoLabel.setText(player + " turn");
         gridAnimation();
+        isDraw = new HashSet<>();
+        infoPane.setVisible(false);
+        newGame.setVisible(false);
 
     }
 
-    public void startNewGame(){
+    public void startNewGame () {
         initialize();
 
+
     }
-    public void drawX0(int i){
-        if(player.equals("X")){
+    public void drawX0 ( int i){
+        if (player.equals("X")) {
             drawX(i);
-        }else{
+        } else {
             draw0(i);
         }
     }
-
-    public void drawX(int i){
-        final int START = 20;
-        final int END = 80;
+    public void drawX ( int i){
+        final int START = 25;
+        final int END = 75;
         Line line1 = new Line(START, START, START, START);
-        line1.setStrokeWidth(3);
+        line1.setStrokeWidth(5);
         line1.setId("XDraw1");        // Create the second diagonal line (from top-right to bottom-left)
         Line line2 = new Line(END, START, END, START);
-        line2.setStrokeWidth(3);
+        line2.setStrokeWidth(5);
         line2.setId("XDraw2");
 
         // Add lines to the root pane
@@ -90,16 +102,16 @@ public class Controller {
         Timeline timeline = new Timeline();
 
         // Add a keyframe to the timeline
-        KeyFrame keyFrame = new KeyFrame(Duration.millis(400),
-                new KeyValue(line1.endYProperty(),  END) ,
-                new KeyValue(line1.startYProperty(),  START),
-                new KeyValue(line1.endXProperty(),  END) ,
-                new KeyValue(line1.startXProperty(),  START),
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(200),
+                new KeyValue(line1.endYProperty(), END),
+                new KeyValue(line1.startYProperty(), START),
+                new KeyValue(line1.endXProperty(), END),
+                new KeyValue(line1.startXProperty(), START),
 
-                new KeyValue(line2.endYProperty(),  END) ,
-                new KeyValue(line2.startYProperty(),  START),
-                new KeyValue(line2.endXProperty(),  START) ,
-                new KeyValue(line2.startXProperty(),  END)
+                new KeyValue(line2.endYProperty(), END),
+                new KeyValue(line2.startYProperty(), START),
+                new KeyValue(line2.endXProperty(), START),
+                new KeyValue(line2.startXProperty(), END)
 
 
         );
@@ -112,13 +124,12 @@ public class Controller {
         // Start the animation
         timeline.play();
     }
-
-    public void draw0(int i){
+    public void draw0 ( int i){
         // Create an arc (round line)
-        Arc arc = new Arc(50, 50, 38, 38, 0, 0);
+        Arc arc = new Arc(50, 50, 33, 33, 0, 0);
         arc.setFill(Color.TRANSPARENT);
         arc.setStroke(Color.web("#F2EBD3"));
-        arc.setStrokeWidth(5);
+        arc.setStrokeWidth(7);
         arc.setType(ArcType.OPEN);
 
         // Add the arc to the root pane
@@ -128,7 +139,7 @@ public class Controller {
         Timeline timeline = new Timeline();
 
         // Add a keyframe to the timeline
-        KeyFrame keyFrame = new KeyFrame(Duration.millis(600),
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(300),
                 new KeyValue(arc.lengthProperty(), -360)
         );
 
@@ -138,7 +149,41 @@ public class Controller {
         // Play the animation
         timeline.play();
     }
-    public void gridAnimation(){
+    public void drawWinLine(int[] winLine){
+        Line line1 = new Line(winLine[0], winLine[1], winLine[0], winLine[1]);
+        line1.setStrokeWidth(7);
+        line1.setId("winLine");
+
+
+        // Add lines to the root pane
+        infoPane.getChildren().addAll(line1);
+        Timeline timeline = new Timeline();
+
+        // Add a keyframe to the timeline
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(500),
+                new KeyValue(line1.endYProperty(), winLine[3]),
+                new KeyValue(line1.startYProperty(), winLine[1]),
+                new KeyValue(line1.endXProperty(), winLine[2]),
+                new KeyValue(line1.startXProperty(), winLine[0])
+        );
+        // Add the keyframe to the timeline
+        timeline.getKeyFrames().add(keyFrame);
+
+        // Set the cycle count for the animation (indefinite for continuous animation)
+        //timeline.setCycleCount(Timeline.INDEFINITE);
+
+        // Start the animation
+        timeline.play();
+    }
+    private void playSound() {
+        /*
+        Media sound = new Media(new File(SOUND_FILE).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.play();
+         */
+        java.awt.Toolkit.getDefaultToolkit().beep();
+    }
+    public void gridAnimation() {
         final int LINELENGTH = 150;
         final double startX = 0.0;
         final double startY = 0.0;
@@ -155,17 +200,17 @@ public class Controller {
 
         // Add a keyframe to the timeline
         KeyFrame keyFrame = new KeyFrame(Duration.millis(700),
-                new KeyValue(topHorLine.endXProperty(), LINELENGTH + topHorLine.getEndX()) ,
-                new KeyValue(topHorLine.startXProperty(),  -(LINELENGTH - topHorLine.getStartX())),
+                new KeyValue(topHorLine.endXProperty(), LINELENGTH + topHorLine.getEndX()),
+                new KeyValue(topHorLine.startXProperty(), -(LINELENGTH - topHorLine.getStartX())),
 
-                new KeyValue(bottomHorLine.endXProperty(), LINELENGTH + bottomHorLine.getEndX()) ,
-                new KeyValue(bottomHorLine.startXProperty(),  -(LINELENGTH - bottomHorLine.getStartX())),
+                new KeyValue(bottomHorLine.endXProperty(), LINELENGTH + bottomHorLine.getEndX()),
+                new KeyValue(bottomHorLine.startXProperty(), -(LINELENGTH - bottomHorLine.getStartX())),
 
-                new KeyValue(leftVerLine.endYProperty(), LINELENGTH + leftVerLine.getEndY()) ,
-                new KeyValue(leftVerLine.startYProperty(),  -(LINELENGTH - leftVerLine.getStartY())),
+                new KeyValue(leftVerLine.endYProperty(), LINELENGTH + leftVerLine.getEndY()),
+                new KeyValue(leftVerLine.startYProperty(), -(LINELENGTH - leftVerLine.getStartY())),
 
-                new KeyValue(rightVerLine.endYProperty(), LINELENGTH + rightVerLine.getEndY()) ,
-                new KeyValue(rightVerLine.startYProperty(),  -(LINELENGTH - rightVerLine.getStartY()))
+                new KeyValue(rightVerLine.endYProperty(), LINELENGTH + rightVerLine.getEndY()),
+                new KeyValue(rightVerLine.startYProperty(), -(LINELENGTH - rightVerLine.getStartY()))
 
         );
         // Add the keyframe to the timeline
@@ -178,85 +223,100 @@ public class Controller {
         timeline.play();
     }
 
+    private void onButtonPressed ( int i){
+
+            if (matrix[i].equals("X") || matrix[i].equals("0") || gameOver) return;
+            playSound();
+            matrix[i] = player;
+            drawX0(i);
+            int result = isWin();
+            if (result != -1) {
+                drawWinLine(WIN_LINE[result]);
+                gameOver(player + " win!");
+                //infoLabel.setText(player + " win!!!!");
+            }else if (isDraw(i)) {
+                gameOver("Draw!");
+                infoLabel.setText("Draw!");
+
+            } else {
+                nextTurn();
+                infoLabel.setText(player + " turn");
+            }
 
 
+        }
 
-
-
-
-
-
-
-
-    private void onButtonPressed(int i){
-
-        if(matrix[i].equals("X") || matrix[i].equals("0") || gameOver) return;
-
-        buttons[i].setText(player);
-        matrix[i] = player;
-        drawX0(i);
-        if(isWin()){
+        public void gameOver(String s){
             gameOver = true;
-            infoLabel.setText(player + " win!!!!");
-        }else{
-            nextTurn();
-            infoLabel.setText(player + " turn");
+            infoPane.setVisible(true);
+            newGame.setVisible(true);
+            Label gameResult = new Label();
+            gameResult.setPrefSize(300.0, 300.0);
+            gameResult.setId("gameResult");
+            gameResult.setText(s);
+            infoPane.getChildren().add(gameResult);
+
+        }
+
+        public int isWin () {
+            if(matrix[0].equals(matrix[1]) && matrix[1].equals(matrix[2]))
+                return 5;
+            if(matrix[3].equals(matrix[4]) && matrix[4].equals(matrix[5]))
+                return 6;
+            if(matrix[6].equals(matrix[7]) && matrix[7].equals(matrix[8]))
+                return 7;
+
+
+            if(matrix[0].equals(matrix[3]) && matrix[3].equals(matrix[6]))
+                return 2;
+            if(matrix[1].equals(matrix[4]) && matrix[4].equals(matrix[7]))
+                return 3;
+            if(matrix[2].equals(matrix[5]) && matrix[5].equals(matrix[8]))
+                return 4;
+
+            if(matrix[0].equals(matrix[4]) && matrix[4].equals(matrix[8]))
+                return 0;
+            if(matrix[2].equals(matrix[4]) && matrix[4].equals(matrix[6]))
+                return 1;
+            return -1;
+        }
+        public boolean isDraw ( int i){
+            isDraw.add(i);
+            return isDraw.size() == 9;
+        }
+
+
+        private void nextTurn () {
+            player = player.equals("X") ? "0" : "X";
+        }
+        public void btn1 () {
+            onButtonPressed(0);
+        }
+        public void btn2 () {
+            onButtonPressed(1);
+        }
+        public void btn3 () {
+            onButtonPressed(2);
+        }
+        public void btn4 () {
+            onButtonPressed(3);
+        }
+        public void btn5 () {
+            onButtonPressed(4);
+        }
+        public void btn6 () {
+            onButtonPressed(5);
+        }
+        public void btn7 () {
+            onButtonPressed(6);
+        }
+        public void btn8 () {
+            onButtonPressed(7);
+        }
+        public void btn9 () {
+            onButtonPressed(8);
         }
 
 
     }
-
-
-
-
-
-
-
-    public boolean isWin(){
-        return (matrix[0].equals(matrix[1]) && matrix[1].equals(matrix[2]) ||
-                matrix[3].equals(matrix[4]) && matrix[4].equals(matrix[5]) ||
-                matrix[6].equals(matrix[7]) && matrix[7].equals(matrix[8]) ||
-
-                matrix[0].equals(matrix[3]) && matrix[3].equals(matrix[6]) ||
-                matrix[1].equals(matrix[4]) && matrix[4].equals(matrix[7]) ||
-                matrix[2].equals(matrix[5]) && matrix[5].equals(matrix[8]) ||
-
-                matrix[0].equals(matrix[4]) && matrix[4].equals(matrix[8]) ||
-                matrix[2].equals(matrix[4]) && matrix[4].equals(matrix[6]) );
-    }
-
-    private void nextTurn(){
-        player = player.equals("X") ? "0" : "X";
-    }
-    public void btn1() {
-        onButtonPressed(0);
-    }
-    public void btn2() {
-        onButtonPressed(1);
-    }
-    public void btn3() {
-        onButtonPressed(2);
-    }
-    public void btn4() {
-        onButtonPressed(3);
-    }
-    public void btn5() {
-        onButtonPressed(4);
-    }
-    public void btn6() {
-        onButtonPressed(5);
-    }
-    public void btn7() {
-        onButtonPressed(6);
-    }
-    public void btn8() {
-        onButtonPressed(7);
-    }
-    public void btn9() {
-        onButtonPressed(8);
-    }
-
-
-
-}
 
